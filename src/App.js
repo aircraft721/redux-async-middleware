8ae76d42d5a289c9f85a10148041dca74cb59f14
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-10-05 14:26:02 +0300

    added the list of users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import UserList from './components/UserList';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				ss
+				<UserList />
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import reducers from './reducers';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+let store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
